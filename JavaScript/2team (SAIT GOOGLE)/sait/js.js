826fbddd912732dadb73678e993c9b5a166fa9e3
Vadim.sokhnenko <wadim.sokhnenko@mail.ru>
2021-05-26 13:57:50 +0300

    26.05.2021 : 13:57:48,74

---
+++ JavaScript/2team (SAIT GOOGLE)/sait/js.js
@@ -0,0 +1,16 @@
+touchActive = false;
+goTo = document.querySelector("#windowForSearch").value
+
+function sendTo(){
+    location.href = "https://www.google.ru/search?q=" + goTo + "&newwindow=1&sxsrf=ALeKk033kfCziFE-5SvNvJLM8u7nfAWDuw%3A1621142073138&source=hp&ei=OaqgYK_YBcnrrgT3y5-IBA&iflsig=AINFCbYAAAAAYKC4SebT2MywWK04YmNa_NQ-nIZNGhVg&oq=d&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBAgAEEMyBAguEEMyAggAMgsIABCxAxDHARCjAjIFCAAQsQMyBQgAELEDMgUIABCxAzICCAA6BwgjEOoCECdQ7Q1Y7Q1gkB5oAXAAeACAAYQBiAGEAZIBAzAuMZgBAKABAaoBB2d3cy13aXqwAQo&sclient=gws-wiz&ved=0ahUKEwjv2JbruM3wAhXJtYsKHfflB0EQ4dUDCAY&uact=5"
+}
+
+function onScreenKeyboard(){
+
+
+    touchActive = true;
+}
+
+//setTimeout(() => {
+//    location.href = "http://127.0.0.1:5500/2team%20(SAIT%20GOOGLE)/dino/index.html"
+//}, 10000);
